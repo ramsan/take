19e6397f29e26e81ac1c3c2c48ca35c2d8e2e00d
Ramesh <rameshram535@gmail.com>
2012-08-02 13:42:38 +0530

    Update config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Demo::Application.initialize!
+
+
+
+ActionMailer::Base.smtp_settings = {
+                          :address   => "smtp.sendgrid.net",
+                           :port      => 587,
+                           :domain    => "yourdomain.com",
+                           :user_name => "mybusinessapp",
+                           :password  => "4emc912",
+                           :authentication => 'plain',
+                           :enable_starttls_auto => true 
+}
